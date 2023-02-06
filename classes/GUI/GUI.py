6e55b49744eda9c77c88aec18c6ca32ef48b0b49
Luka Zaitch <lzaitch@gmail.com>
2023-02-06 14:17:24 +0100

    addons

--- classes/GUI/GUI.py
+++ classes/GUI/GUI.py
@@ -1,8 +1,10 @@
-from PySide2.QtWidgets import QApplication, QLabel
+from PySide2.QtWidgets import QApplication, QLabel, QTextEdit
 
 app = QApplication([])
 
-
+text_edit = QTextEdit()
 label = QLabel("Hello World!")
+text_edit.textChanged.connect(lambda: label.setText(text_edit.toPlainText()))
+text_edit.show()
 label.show()
 app.exec_()
