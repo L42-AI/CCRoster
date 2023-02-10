2b5c704a0a7baf28fa3a75c0a872d5fa92e1ba1b
Luka Zaitch <lzaitch@gmail.com>
2023-02-10 21:41:58 +0100

    Create first page of gui

--- classes/GUI/GUI.py
+++ classes/GUI/GUI.py
@@ -1,19 +1,101 @@
-from PySide2.QtWidgets import QApplication, QLabel, QTextEdit, QMainWindow, QPushButton
-
 import sys
+from PySide2.QtWidgets import QApplication, QDialog, QListWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout
 
-app = QApplication(sys.argv)
-window = QMainWindow()
-window.setWindowTitle("Scheduly")
+class TimeSlotSetupDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-text_edit = QTextEdit()
-label = QLabel("Hello World!")
-text_edit.textChanged.connect(lambda: label.setText(text_edit.toPlainText()))
+        self.time_slots = []
 
-button = QPushButton()
-button.setText("Your mom")
+        self.setWindowTitle("Time Slot Setup")
 
-text_edit.show()
-label.show()
-# window.show()
-app.exec_()
+        self.time_slot_list = QListWidget()
+        self.time_slot_list.itemDoubleClicked.connect(self.edit_time_slot)
+
+        self.start_time_label = QLabel("Start Time:")
+        self.start_time_edit = QLineEdit()
+
+        self.end_time_label = QLabel("End Time:")
+        self.end_time_edit = QLineEdit()
+
+        self.add_time_slot_button = QPushButton("Add Time Slot")
+        self.add_time_slot_button.clicked.connect(self.add_time_slot)
+
+        self.edit_time_slot_button = QPushButton("Edit Time Slot")
+        self.edit_time_slot_button.clicked.connect(self.edit_selected_time_slot)
+        self.edit_time_slot_button.setEnabled(False)
+
+        self.delete_time_slot_button = QPushButton("Delete Time Slot")
+        self.delete_time_slot_button.clicked.connect(self.delete_selected_time_slot)
+        self.delete_time_slot_button.setEnabled(False)
+
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.add_time_slot_button)
+        button_layout.addWidget(self.edit_time_slot_button)
+        button_layout.addWidget(self.delete_time_slot_button)
+
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.time_slot_list)
+        layout.addWidget(self.start_time_label)
+        layout.addWidget(self.start_time_edit)
+        layout.addWidget(self.end_time_label)
+        layout.addWidget(self.end_time_edit)
+        layout.addLayout(button_layout)
+
+        self.setLayout(layout)
+
+    def add_time_slot(self):
+        start_time = self.start_time_edit.text()
+        end_time = self.end_time_edit.text()
+        self.time_slots.append((start_time, end_time))
+        self.time_slot_list.addItem(f"{start_time} - {end_time}")
+        self.start_time_edit.clear()
+        self.end_time_edit.clear()
+
+    def edit_time_slot(self, item):
+        index = self.time_slot_list.row(item)
+        start_time, end_time = self.time_slots[index]
+        self.start_time_edit.setText(start_time)
+        self.end_time_edit.setText(end_time)
+        self.edit_time_slot_button.setEnabled(True)
+        self.delete_time_slot_button.setEnabled(True)
+
+    def edit_selected_time_slot(self):
+        selected_item = self.time_slot_list.currentItem()
+        if selected_item is not None:
+            index = self.time_slot_list.row(selected_item)
+
+            new_start_time = self.start_time_edit.text()
+            new_end_time = self.end_time_edit.text()
+
+            self.time_slots[index] = new_start_time, new_end_time
+            self.time_slot_list.takeItem(index)
+            self.time_slot_list.addItem(f"{new_start_time} - {new_end_time}")
+
+    def delete_selected_time_slot(self):
+        selected_item = self.time_slot_list.currentItem()
+        if selected_item is not None:
+
+            # Find index
+            index = self.time_slot_list.row(selected_item)
+
+            # Remove from list
+            del self.time_slots[index]
+
+            # Remove item from list box
+            self.time_slot_list.takeItem(index)
+
+            # Disable buttons
+            self.edit_time_slot_button.setEnabled(False)
+            self.delete_time_slot_button.setEnabled(False)
+
+            # Reset text to nothing
+            self.start_time_edit.setText('')
+            self.end_time_edit.setText('')
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    dialog = TimeSlotSetupDialog()
+    dialog.show()
+    sys.exit(app.exec_())
