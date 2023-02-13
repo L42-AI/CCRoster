8bc6f97628234f5badf74b06929a9e9c4325c02f
Luka Zaitch <lzaitch@gmail.com>
2023-02-13 14:26:57 +0100

    Added attributes and get methods

--- classes/representation/employee.py
+++ classes/representation/employee.py
@@ -1,13 +1,30 @@
 import random
 
 class Employee:
-    def __init__(self, name, av, exp) -> None:
+    def __init__(self, name, av, maximum, wage, onboarding) -> None:
         self.name = name
         self.availability = av
-        self.expertise = exp
+        self.wage = wage
+        self.weekly_max = maximum
+        self.onboarding = onboarding
 
-    def get_av(self):
-        return random.choice(self.availability)
+    """ Get """
 
-    def get_name(self):
+    def get_av(self) -> list:
+        if len(self.availability) > 0:
+            return random.choice(self.availability)
+
+    def get_name(self) -> str:
         return self.name
+
+    def get_wage(self) -> float:
+        return float(self.wage)
+
+    def get_onboarding(self) -> bool:
+        return self.onboarding
+
+    def get_week_max_dict(self) -> dict:
+        return self.weekly_max
+
+    def get_week_max(self, week) -> int:
+        return self.weekly_max.get(week)
