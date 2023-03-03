d660e4547b0b51b71a55f98f2f8f16bd8c815fe6
Jacob Groot <jacobgroot79@gmail.com>
2023-03-03 15:30:07 +0100

    commit

--- classes/representation/controller.py
+++ classes/representation/controller.py
@@ -1,11 +1,14 @@
 from classes.representation.employee import Employee
 from classes.representation.shift import Shift
+from data.queries import db_cursor
 
 class Controller:
     def __init__(self, location) -> None:
         self.employee_list: list[Employee] = []
         self.shift_list: list[Shift] = []
         self.location = location
+        self.db, self.cursor = db_cursor()
+        self.threads = []
 
     """ Get """
 
@@ -23,20 +26,18 @@ class Controller:
 
     """ Methods """
 
-    def create_employee(self, first_name, last_name, hourly_wage, level, roles):
-        self.employee_list.append(
-            Employee(
+    def create_employee(self, first_name, last_name, hourly_wage, level, tasks):
+        employee = Employee(
             first_name = first_name,
             last_name = last_name,
             av = [],
             maximum={},
             wage = hourly_wage,
-            onboarding = level,
-            roles = roles
+            level = level,
+            task = task
             )
-        )
-
-        self.communicate_server()
+        self.employee_list.append(employee)
+        self.threads.append(("INSERT INTO Employee (name, hourly, level, task, location) VALUES (%s, %s, %s, %s, %s)", ))
 
     def edit_employee_availability(self, employee: Employee, availability_slot: list[int], add: bool):
         for employee_instance in self.employee_list:
@@ -92,9 +93,9 @@ class Controller:
         
         self.communicate_server()
 
-    def communicate_server(self):
+    def communicate_server(self, method, input):
         """ Function that gets called all the time to send the new data to the server """
-        pass
+
 
 from classes.representation.employee import Employee
 from classes.representation.shift import Shift
