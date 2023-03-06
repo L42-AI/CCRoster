743e28fb0dc730965e63db50d54755694fd653b3
Jacob Groot <jacobgroot79@gmail.com>
2023-03-06 12:34:31 +0100

    small update, consillium is now called shifter

--- main_GUI.py
+++ main_GUI.py
@@ -2,9 +2,10 @@ from classes.GUI.GUI import MainWindow
 from PySide6.QtWidgets import QApplication
 import sys
 from classes.representation.generator import Generator
-
+from classes.representation.controller import Controller
 if __name__ == "__main__":
-    Generator()
+    generator = Generator()
+    controller = Controller(generator)
     window = QApplication(sys.argv)
     app = MainWindow()
     app.show()
