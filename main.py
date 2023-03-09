448e34c060d2835d7cc529e01ddc19e2ec2e03dc
Luka Zaitch <lzaitch@gmail.com>
2023-03-09 19:37:45 +0100

    Made test main for advanced GUI

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from classes.representation.generator import Generator
+from PySide6.QtCore import Signal
+from PySide6.QtWidgets import (QApplication, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit,
+                               QPushButton, QWidget, QListWidget, QComboBox,
+                               QCheckBox, QListWidgetItem)
+import re
+import sys
+# from classes.representation.controller import Controller
+# from classes.representation.generator import Generator
+# from classes.GUI.GUI_shiftsheet import ShiftSheetViewer
+from classes.GUI.GUI_shiftsheet import ShiftSheet
 
 if __name__ == "__main__":
-    Generator()
+    # generator = Generator()
+    # controller = Controller(1)
+    app = QApplication(sys.argv)
+    window = ShiftSheet()
+    window.show()
+    # app = ShiftSheetViewer(controller)
+    # app.show()
+    sys.exit(app.exec())
