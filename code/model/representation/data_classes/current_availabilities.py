ff8e1ef5736a52fe97633039959845cdead77540
Luka <lzaitch@gmail.com>
2023-05-11 17:53:05 +0200

    Implement current av

---
+++ code/model/representation/data_classes/current_availabilities.py
@@ -0,0 +1,7 @@
+
+from model.representation.behaviour_classes.schedule_constants import total_availabilities
+
+class CurrentAvailabilities(dict):
+    def __init__(self):
+        for shift_id, shift_availability in total_availabilities.items():
+            self[shift_id] = [0, shift_availability]
