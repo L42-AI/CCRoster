753c298099701126d151cc0cf6fe4e5741396840
Luka <lzaitch@gmail.com>
2023-06-08 23:04:58 +0200

    Set to only model and added standard functions

--- code/model/model.py
+++ code/model/model.py
@@ -12,6 +12,7 @@ from model.manipulate.fill import Fill, Greedy
 from model.manipulate.PPA import PPA
 
 class Model():
+
     """ DATABASE """
     def upload(shift_list, employee_list):
         ...
@@ -20,15 +21,38 @@ class Model():
         ...
     
     """ SCHEDULE """
+    def get_standard_cost(employee_list: list[Employee]) -> float:
+        total = sum([employee.get_minimal_hours() * employee.get_wage()  for employee in employee_list])
 
-    def create_random_schedule(**config):
-        return Fill.fill(AbsSchedule(Workload(config['shifts'], config['employees'])))
+        return round(total, 2)
+    
+    def get_total_availabilties(employee_list: list[Employee], shift_list: list[Shift]) -> dict:
+        return {shift.id: Shiftconstraints.get_employee_set(shift, employee_list) for shift in shift_list}
 
-    def create_greedy_schedule(**config):
-        return Greedy.fill(config['employees'], config['shifts'], Schedule(Workload(config['shifts'], config['employees']), CurrentAvailabilities()))
+    def get_coliding_dict(shifts_list: list[Shift]) -> dict[int, set[int]]:
 
-    def propagate(**config):
+        time_conflict_dict = {shift.id: set() for shift in shifts_list}
 
+        for i, shift_1 in enumerate(shifts_list):
+
+            for shift_2 in shifts_list[i+1:]:
+                if shift_1.end < shift_2.start:
+                    continue
+                if shift_1.start < shift_2.end:
+                    time_conflict_dict[shift_1.id].add(shift_2.id)
+                    time_conflict_dict[shift_2.id].add(shift_1.id)
+
+        return time_conflict_dict
+
+    def create_random_schedule(**config) -> AbsSchedule:
+        Filler = Fill(Shiftconstraints(config['shifts'], config['employees']))
+        return Filler.fill(AbsSchedule(Workload(config['shifts'], config['employees'])))
+
+    def create_greedy_schedule(**config) -> AbsSchedule:
+        Filler = Greedy(Shiftconstraints(config['shifts'], config['employees']))
+        return Filler.fill(config['employees'], config['shifts'], Schedule(Workload(config['shifts'], config['employees']), CurrentAvailabilities(config['total_availabilities'])))
+
+    def propagate(**config) -> AbsSchedule:
         # create a shift constraints instance that will be used by PPA and Fill
         shift_constraints = Shiftconstraints(config['shifts'], config['employees'])
         config['shiftconstraints'] = shift_constraints
@@ -41,28 +65,4 @@ class Model():
         schedule = P.grow()
 
         return schedule
-
-class Generator(Model):
-
-    def create_random_schedule():
-        return Fill.fill(AbsSchedule(Workload()))
-
-    def create_greedy_schedule(employee_list: list[Employee], shift_list: list[Shift]):
-        return Greedy.fill(employee_list, shift_list, Schedule(Workload(), CurrentAvailabilities()))
-
-    def propagate(employee_list: list[Employee], shift_list: list[Shift], **kwargs):
-
-        # create a shift constraints instance that will be used by PPA and Fill
-        shift_constraints = Shiftconstraints(kwargs['shifts'], kwargs['employees'])
-        kwargs['shiftconstraints'] = shift_constraints
-
-        # create Fill
-        FillClass = Fill(shift_constraints)
-        schedule = FillClass.fill(AbsSchedule(Workload(shift_list, employee_list)))
-
-        P = PPA(schedule, **kwargs)
-        schedule = P.grow()
-
-        return schedule
-    
 
