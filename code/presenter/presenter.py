a517dfed64911d911b4642a3082fb1aea9ec35eb
Luka <lzaitch@gmail.com>
2023-06-09 14:17:16 +0200

    Bugfix and added optimal

--- code/presenter/presenter.py
+++ code/presenter/presenter.py
@@ -6,7 +6,7 @@ class Presenter:
 
     def __init__(self, config) -> None:
         self.config = config
-        self.employee_list, self.shift_list = self._retrieve_data(self.config)
+        self.shift_list, self.employee_list = self._retrieve_data(self.config)
         self.id_employee_dict = {x.id : x for x in self.employee_list}
         self.id_shift_dict = {x.id : x for x in self.shift_list}
 
@@ -18,12 +18,12 @@ class Presenter:
 
     def _retrieve_data(self, config):
         match config['datatype']:
-            case 'assign':
+            case 'offline':
                 return Model.get_offline_data()
             case 'online':
                 return download(config['session_id'])
             case other:
-                raise ValueError(f"{config['datatype']} not valid! Choose from: 'assign' or 'online'")
+                raise ValueError(f"{config['datatype']} not valid! Choose from: 'offline' or 'online'")
 
     def _build_schedule(self, config):
         match config['runtype']:
@@ -33,5 +33,7 @@ class Presenter:
                 return Model.create_greedy_schedule(self.employee_list, self.shift_list)
             case 'propagate':
                 return Model.propagate(self.employee_list, self.shift_list, **config)
+            case 'optimal':
+                return Model.optimal(self.employee_list, self.shift_list, **config)
             case other:
                 raise ValueError(f"{config['runtype']} not valid! Choose from: 'greedy', 'random' or 'propagate'")
